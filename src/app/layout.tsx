504a2606345c2f6f2d9c4fc009aba595dfdfe95a
James Diffey <diffeyj@outlook.com>
2025-01-02 14:28:32 +0000

    remove boilerplate

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "React project",
+  description: "James Diffey - front-end assessment",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Home() {
+  return <div className="h-[100vh] bg-black text-white">hi</div>;
+}
